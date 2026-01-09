bcfe2d618811119ed3c2fcbdeb1afe9b5111647f
xonixx <xonixx@gmail.com>
2026-01-09 15:13:48 +0200

    Add javadoc

--- src/main/java/saner_jdbc/SqlArgs.java
+++ src/main/java/saner_jdbc/SqlArgs.java
@@ -6,6 +6,17 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
+/**
+ * This class simplifies the management of a complex list of arguments in PreparedStatement. You
+ * simply use <code>.arg(value)</code> to add arguments to an internal list instead of question
+ * marks, and then <code>.setArgs()</code> once to apply the list instead of a list of <code>
+ * .set[data_type]()</code> calls.
+ *
+ * <p>This is particularly useful to simplify code for "dynamic" SQL queries, i.e., when the shape
+ * of the query is determined by conditions.
+ *
+ * <p>See <a href="https://maximullaris.com/saner_jdbc.html">article</a>.
+ */
 public class SqlArgs {
   private final List<Object> args = new ArrayList<>();
 
